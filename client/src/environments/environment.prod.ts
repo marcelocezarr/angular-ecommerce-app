b835e6ed43afb6ce22ecef126f070bc33a823061
Pedro Brantis <pedro.paulo@fpf.br>
2025-10-27 21:01:51 -0400

    Changes 3

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
-const host = window.location.hostname; // pega o IP/DNS da VM
 export const environment = {
   production: true,
-  apiUrl: `http://${host}:3000/api/v1/`,
+  apiUrl: `http://backend:3000/api/v1/`,
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: `http://localhost:30020/api/v1/`,
+  production: true,
+  apiUrl: `http://backend:3000/api/v1/`,
 };
